f962b2e90dbd6d02e2405720ffe20c05be1550b2
mo-hassann <mohamed.hassan.bu@gmail.com>
2024-08-25 14:53:16 +0300

    fix issue

--- client/todo/api/use-get-todo.ts
+++ client/todo/api/use-get-todo.ts
@@ -6,12 +6,13 @@ export default function useGetTodo(id?: string) {
   const query = useQuery({
     queryKey: ["todo", id],
     queryFn: async () => {
-      const res = await client.api.todo[":id"].$get({ param: { id } });
+      const res = await client.api.todo[":id"].$get({ param: { id: id as string } });
       if (!res.ok) throw new Error("field to fetch accounts");
 
       const { data } = await res.json();
       return data;
     },
+    enabled: !!id,
   });
 
   return query;
